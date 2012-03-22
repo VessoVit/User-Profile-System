eb60361f747ec423e867ccd43988958d6374886f
Vesso <v.vitanov@gmail.com>
2012-03-22 21:23:48 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 define("DB_SERVER", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "root");
-define("DB_NAME", "dda_partone");
+define("DB_USER", "");
+define("DB_PASS", "");
+define("DB_NAME", "");
 
 define("USER_TABLE", "ddaUsers");
 define("REL_TABLE", "ddaRels");
 define("TERM_TABLE", "ddaSearch_log");
 
-define("HOME", "www.vv-studio.com/dda");
+define("HOME", "");
 ?>
